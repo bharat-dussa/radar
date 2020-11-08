b2a06f7b655deedad7deb82f02d1ecefd7650000
bharat-dussa <bharatdussa@gmail.com>
2020-11-08 14:48:57 +0530

    adding client

--- security/keys/keys.js
+++
@@ -1,9 +0,0 @@
-//mongodb+srv://6dGE7WQrdSwTTq1N:<password>@instagram-main.dmhku.mongodb.net/<dbname>?retryWrites=true&w=majority
-//xtMB3cewUKjocPoy
-// keys for mongoDB connection mongoURI
-module.exports={
-    MONGOURI:"mongodb+srv://6dGE7WQrdSwTTq1N:xtMB3cewUKjocPoy@instagram-main.dmhku.mongodb.net/<dbname>?retryWrites=true&w=majority",
-    JWT_SECRET:"zsxdcfvgbhnjmkl",
-    SECRET_KEY:"poiuytrtyuiop56#$"
-
-}
